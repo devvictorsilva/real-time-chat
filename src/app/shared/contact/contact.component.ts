2a230183b9a2cc51928c52d7ade30cc02a740f94
Victor <paulo9.victor.silva@gmail.com>
2024-12-29 06:32:52 -0300

    feat: contact component

---
+++ src/app/shared/contact/contact.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { ContactResponse } from '../../core/interfaces/contact';
+
+@Component({
+  selector: 'chat-contact',
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+  @Input() contact!: ContactResponse
+}
