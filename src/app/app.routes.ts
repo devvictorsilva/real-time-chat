e133050d3202212303542f59d1a43dc51736901c
Victor <paulo9.victor.silva@gmail.com>
2024-12-31 15:44:28 -0300

    fix: component on / route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { ChatComponent } from './pages/chat/chat.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ChatComponent,
+    component: MainComponent,
   }
 ];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ChatComponent } from './chat/chat.component';
 import { ContactComponent } from './contact/contact.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, ContactComponent],
+  declarations: [DashboardComponent, ChatComponent, ContactComponent],
   imports: [
     CommonModule
   ],
